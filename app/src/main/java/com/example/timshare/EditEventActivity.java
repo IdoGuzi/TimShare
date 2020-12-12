e703beb4cc25a97229ddbf56f2ca5a2e2cd91e45
ShirelIsr <shirel.isr@gmail.com>
2020-12-12 23:44:26 +0200

    day

--- app/src/main/java/com/example/timshare/EditEventActivity.java
+++ app/src/main/java/com/example/timshare/EditEventActivity.java
@@ -1,54 +1,26 @@
 package com.example.timshare;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.text.format.DateFormat;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
-
-import java.text.BreakIterator;
-import java.text.ParseException;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
 
 import classes.Date;
-import classes.Event;
-import classes.PUser;
-import interfaces.event;
-import interfaces.user;
 
-
-public class EventActivity extends AppCompatActivity {
-
+public class EditEventActivity extends AppCompatActivity {
     private static final String TAG = "EventActivity";
 
     private FirebaseDatabase db = FirebaseDatabase.getInstance();
@@ -59,35 +31,19 @@ public class EventActivity extends AppCompatActivity {
     private int startYear,startMonth,startDay,startHour,startMin;
     private Button cancelBtn, saveBtn;
     private FirebaseAuth fAuth;
-
-    private Calendar calendar;
-
     private String startDate, endDate, endTime, startTime, location, eventName="My event", Description;
     private DatePickerDialog startDatePickerDialog, endDatePickerDialog;
     private TimePickerDialog startTimePickerDialog, endTimePickerDialog;
     private Date dateStart, dateEnd;
     private DatabaseReference mDatabase;
-
+    private String eventID;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private FirebaseAuth users_data = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_event);
-
-
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-      /*  FirebaseAuth fAuth = FirebaseAuth.getInstance();
-        FirebaseUser fbuser = fAuth.getCurrentUser();
-
-        String userId=fbuser.getUid();
-        fdb= fdb.child(fbuser.getUid());
-
-        /*
-        fdb.child("Events").child(eventID).setValue(ven);
-        fdb.child("Users").child(ven.getOwnerID).addEvent(eventID)
-*/
-
-
+        setContentView(R.layout.activity_edit_event);
         startEventDateViewText = findViewById(R.id.startEventDateViewText);
         endEventDateViewText = findViewById(R.id.endEventDateViewText);
         startEventTimeViewText = findViewById(R.id.startEventTimeViewText);
@@ -98,34 +54,15 @@ public class EventActivity extends AppCompatActivity {
         cancelBtn = findViewById(R.id.cancleButton);
         saveBtn = findViewById(R.id.saveButton);
 
-        eventName = editTextEventName.getText().toString().trim();
-        location = editTextLocation.getText().toString().trim();
-        Description = editTextDescription.getText().toString().trim();
+        if (getIntent().hasExtra("com.example.timshare.DATE")) {
+            eventID = getIntent().getExtras().getString("com.example.timshare.DATE");
+        }
+        DatabaseReference ref = database.getReference();
+        FirebaseUser user = users_data.getCurrentUser();
+        ref = ref.child("Users").child(user.getUid());
 
-        calendar = Calendar.getInstance();
-        myYear = calendar.get(Calendar.YEAR);
-        myMonth = calendar.get(Calendar.MONTH);
-        myDay = calendar.get(Calendar.DAY_OF_MONTH);
-        myHourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
-        myMinute = calendar.get(Calendar.MINUTE);
 
-        endYear= startYear= myYear;
-        startMonth=endMonth=myMonth;
-        startDay=endDay=myDay;
-        startHour=endHour=myHourOfDay;
-        startMin=endMinute=myMinute;
 
-        startTime  = myHourOfDay + ":" + myMinute;
-        if(myHourOfDay!=23)
-            endHour=myHourOfDay+1;
-        //startDate = endDate = myDay + "/" + (myMonth+1) + "/" + myYear;
-        startEventTimeViewText.setText( myHourOfDay + ":" + myMinute);
-        startEventDateViewText.setText(myDay + "/" + (myMonth+1) + "/" + myYear);
-        endEventTimeViewText.setText( endHour + ":" + myMinute);
-        endEventDateViewText.setText(myDay + "/" + (myMonth+1) + "/" + myYear);
-
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        final String uid = user.getUid();
 
         //time and date
         startEventDateViewText.setOnClickListener(new View.OnClickListener() {
@@ -233,64 +170,10 @@ public class EventActivity extends AppCompatActivity {
         cancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myIntent = new Intent(EventActivity.this, CalendarActivity.class);
+                Intent myIntent = new Intent(EditEventActivity.this, DayActivity.class);
                 startActivity(myIntent);
 
             }
         });
-
-        saveBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //save the date
-                dateStart =new Date(startYear,startMonth,startDay,startHour,startMin);
-                dateEnd =new Date(endYear,endMonth,endDay, endHour, endMinute);
-
-                mDatabase.child("Users").child(uid).addListenerForSingleValueEvent(
-                        new ValueEventListener() {
-                            @Override
-                            public void onDataChange(DataSnapshot dataSnapshot) {
-                                // Get user value
-                                // [START_EXCLUDE]
-                                if (user == null) {
-                                    // User is null, error out
-                                    Log.e(TAG, "User " + uid + " is unexpectedly null");
-                                    Toast.makeText(EventActivity.this,
-                                            "Error: could not fetch user.",
-                                            Toast.LENGTH_SHORT).show();
-                                } else {
-                                    // Write new post
-                                    //PUser user = dataSnapshot.getValue(PUser.class);
-                                    updatedFields();
-                                    writeNewEvent(uid, eventName, Description,location, dateStart, dateEnd);
-                                }
-                            }
-
-                            @Override
-                            public void onCancelled(DatabaseError databaseError) {
-                                Log.w(TAG, "getUser:onCancelled", databaseError.toException());
-                            }
-                        });
-                Intent myIntent = new Intent(EventActivity.this, CalendarActivity.class);
-                startActivity(myIntent);
-            }
-
-        });
-    }
-
-    private void writeNewEvent(String ownerID, String eventName, String eventDescription,String eventLocation, Date startingDate, Date endingDate) {
-        DatabaseReference ref = mDatabase.getRef();
-        ref =ref.child("Events");
-        DatabaseReference eventRef=ref.push();
-        String key=eventRef.getKey();
-        event myEvent = new Event(ownerID,key, eventName,eventDescription,eventLocation,startingDate,endingDate);
-        eventRef.setValue(myEvent);
-        mDatabase.child("Users").child(ownerID).child("events").child(key).setValue(true);
-    }
-
-    private void updatedFields(){
-        eventName = editTextEventName.getText().toString().trim();
-        location = editTextLocation.getText().toString().trim();
-        Description = editTextDescription.getText().toString().trim();
     }
 }
